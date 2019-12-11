851a8264afccf1637d2b624684ae5a938c5d8177
Khanh Hung Nguyen <57281641+hungnk1112k@users.noreply.github.com>
2019-12-11 14:47:19 +0700

    Update all_task.py

--- all_task.py
+++ all_task.py
@@ -1,15 +1,14 @@
 import math
 import socket
 import time
-import sys
 import string
 import base64
 import zlib
 
-while 1:
-    task = int(input("Enter your task\n"))
-    if 1 <= task <= 4:
-        break
+# while 1:
+#     task = int(input("Enter your task\n"))
+#     if 1 <= task <= 4:
+#         break
 
 def solve1(txt):
     n1, n2 = txt.split(' / ')
@@ -44,48 +43,64 @@ irc.send("JOIN {}\n".format(channel).encode('utf-8'))
 
 sent_private_msg = False
 sent_result = False
+final_code = []
 
-while 1:
-    text = ''
-    time.sleep(0.1)
-    try:
-        text = irc.recv(2040).decode('utf-8')
-        if text:
-            print(text)
-    except Exception:
-        pass
-    
-    if 'BANNED' in text:
-        print('sleep for 30 seconds')
-        timer = 0
-        while timer < 31:
-            print(timer)
-            time.sleep(1)
-            timer += 1
-        sent_private_msg = False
-        sent_result = False
-        text = 'End of /NAMES list.'
-    if 'PING' in text:
-        msg = 'PONG {} \r\n'.format(text.split()[1])
-        irc.send(msg.encode('utf-8'))
-    if text.strip().endswith('End of /NAMES list.') and not sent_private_msg: # haven't sent private message yet
-        # :irc.hackerzvoice.net 366 qiwoehrwqero123 #root-me_challenge :End of /NAMES list.
-        print('send message')
-        msg = 'PRIVMSG candy : !ep{}\r\n'.format(task)
-        irc.send(msg.encode('utf-8'))
-        sent_private_msg = True
-    if ':Candy!Candy@root-me.org' in text and sent_private_msg and not sent_result: # sent private message but not result
-        print('send result')
-        result = locals()['solve{}'.format(task)](text.strip().split(':')[-1])
-        msg = 'PRIVMSG candy : !ep{} -rep {}\r\n'.format(task, result)
-        print(result)
-        irc.send(msg.encode('utf-8'))
-        print(msg)
-        sent_result = True
-        continue
-    if text != '' and sent_private_msg and sent_result:
-        print('send quit')
-        msg = 'QUIT :Bye!\r\n'
-        irc.send(msg.encode('utf-8'))
-        irc.close()
-        sys.exit('End')
+for task in range (1,5):
+    while 1:
+        if task == 1:
+            text = ''
+        else:
+            text = 'End of /NAMES list.'
+        
+        time.sleep(0.1)
+        try:
+            text = irc.recv(2040).decode('utf-8')
+            if text:
+                print(text)
+        except Exception:
+            pass
+            
+        if 'BANNED' in text:
+            print('sleep for 30 seconds')
+            timer = 0
+            while timer < 31:
+                print(timer)
+                time.sleep(1)
+                timer += 1
+            sent_private_msg = False
+            sent_result = False
+            text = 'End of /NAMES list.'
+        if 'PING' in text:
+            msg = 'PONG {} \r\n'.format(text.split()[1])
+            irc.send(msg.encode('utf-8'))
+        if text.strip().endswith('End of /NAMES list.') and not sent_private_msg: # haven't sent private message yet
+            # :irc.hackerzvoice.net 366 qiwoehrwqero123 #root-me_challenge :End of /NAMES list.
+            print('send message')
+            msg = 'PRIVMSG candy : !ep{}\r\n'.format(task)
+            irc.send(msg.encode('utf-8'))
+            print(msg)
+            sent_private_msg = True
+        if ':Candy!Candy@root-me.org' in text and sent_private_msg and not sent_result: # sent private message but not result
+            print('send result')
+            result = locals()['solve{}'.format(task)](text.strip().split(':')[-1])
+            msg = 'PRIVMSG candy : !ep{} -rep {}\r\n'.format(task, result)
+            print(result)
+            irc.send(msg.encode('utf-8'))
+            print(msg)
+            sent_result = True
+            continue
+        if 'You dit it!' in text and sent_private_msg and sent_result:
+            final_code.append(text.strip().split(' ')[-1])
+            if task == 4:
+                print('send quit')
+                msg = 'QUIT :Bye!\r\n'
+                irc.send(msg.encode('utf-8'))
+                irc.close()
+                break
+            else:
+                sent_private_msg = False
+                sent_result = False
+                break
+
+for i in range(1,5):
+    print('Code for challenge ' + str(i) + ' is ' + final_code[i-1])
